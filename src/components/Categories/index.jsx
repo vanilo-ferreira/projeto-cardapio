b80fd17dc47ebe1538430e6971566072dd91e810
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-31 19:26:57 -0300

    feat: Adicionando classes no componente Categories

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
+import styles from "./Categories.module.css";
+
 import IconeEntradas from "../../../public/assets/entrada.png";
 import IconeMassas from "../../../public/assets/massa.png";
 import IconeCarnes from "../../../public/assets/carne.png";
@@ -10,40 +12,55 @@ import IconeSobremesas from "../../../public/assets/sobremesa.png";
 
 const Categories = () => {
   return (
-    <div>
+    <div className={styles.conteinerCategories}>
       <button>
         <Image
           src={IconeEntradas}
-          width={20}
-          height={20}
+          className={styles.categoryImage}
           alt="Icone Entradas"
         />
-      </button>
-      <button>
-        <Image src={IconeMassas} width={20} height={20} alt="Icone Massas" />
-        Massas
-      </button>
-      <button>
-        <Image src={IconeCarnes} width={20} height={20} alt="Ícone Carnes" />
-        Carnes
-      </button>
-      <button>
-        <Image src={IconeBebidas} width={20} height={20} alt="Icone Bebidas" />
-        <img src={IconeBebidas} alt="" />
-        Bebidas
-      </button>
-      <button>
-        <Image src={IconeSaladas} width={20} height={20} alt="Icone Saladas" />
-        Saladas
+        <span className={styles.buttonText}>Entradas</span>
       </button>
       <button>
         <Image
+          src={IconeMassas}
+          className={styles.categoryImage}
+          alt="Icone Massas"
+        />
+        <span className={styles.buttonText}>Massas</span>
+      </button>
+      <button>
+        <Image
+          src={IconeCarnes}
+          className={styles.categoryImage}
+          alt="Ícone Carnes"
+        />
+        <span className={styles.buttonText}>Carnes</span>
+      </button>
+      <button>
+        <Image
+          src={IconeBebidas}
+          className={styles.categoryImage}
+          alt="Icone Bebidas"
+        />
+        <img src={IconeBebidas} alt="" />
+        <span className={styles.buttonText}>Bebidas</span>
+      </button>
+      <button>
+        <Image
+          className={styles.categoryImage}
+          src={IconeSaladas}
+          alt="Icone Saladas"
+        />
+        <span className={styles.buttonText}>Saladas</span>
+      </button>
+      <button>
+        <Image
+          className={styles.categoryImage}
           src={IconeSobremesas}
-          width={20}
-          height={20}
           alt="Icone Sobremesas"
         />
-        Sobremesas
+        <span className={styles.buttonText}>Sobremesas</span>
       </button>
     </div>
   );
