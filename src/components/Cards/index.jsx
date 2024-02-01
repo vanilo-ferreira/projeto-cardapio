90b14ad469f134645d67e873784c6122c5951a89
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-01-31 22:14:23 -0300

    feat: Ajustar valores dos produtos no componente Cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -5,21 +5,28 @@ import Image from "next/image";
 const Card = (props) => {
   return (
     <div className={styles.card}>
-        <Image
-          className={styles.cardImage}
-          src={props.imagem}
-          alt="Imagem do produto"
-        />
+      <Image
+        className={styles.cardImage}
+        src={props.imagem}
+        alt="Imagem do produto"
+      />
       <div className={styles.cardText}>
         <h3>{props.nome}</h3>
         <h4>{props.categoria}</h4>
         <p>{props.descricao}</p>
         <div>
-          <p>R$ {props.preco}</p>
+          <p>
+            {new Intl.NumberFormat("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            }).format(props.preco)}
+          </p>
         </div>
       </div>
     </div>
   );
 };
+
+
 
 export default Card;
